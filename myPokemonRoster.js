27234c6650987c4ae4335c7feb8147a2936a0bb8
taneilASC4 <bluefiredragonz17@gmail.com>
2017-07-11 10:43:00 -0700

    first commit

---
+++ myPokemonRoster.js
@@ -0,0 +1,15 @@
+var myPokemonRoster = ["infernape", "luxray"];
+var firsttofight = myPokemonRoster [0];  
+var secondtofight = myPokemonRoster [1];
+
+
+myPokemonRoster.push("lucario");
+myPokemonRoster.push("haunter");
+
+myPokemonRoster.length;
+var size = myPokemonRoster.length;
+//console.log(size);
+
+for (var i=0; i<4; i++){
+    console.log(myArray[i]);
+}
---
+++ taneil.js
@@ -0,0 +1,8 @@
+console.log("Taneil is awesome");
+
+console.log("hello world");
+
+var i=0 i<6 i++{
+    console.log("bob", "mahdi", "cyril", "juan", "mike");
+}
+
